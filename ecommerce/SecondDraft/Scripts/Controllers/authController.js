9a780ba5fdc83fbdac51678f7fc6d531ee9c5873
Kaushik Amartya <kaushama@ap.dtcs.local>
2025-02-11 21:33:19 +0530

    Deleted unnecessary files

--- ecommerce/SecondDraft/Scripts/Controllers/authController.js
+++
@@ -1,8 +0,0 @@
-// import credentials from "../dal/credentials"
-
-function validateLogin(email, password){
-    // const user = credentials.find(u => u.name == email);
-    // if(!user) return user;
-    // return user.password == password;
-    return true;
-}
